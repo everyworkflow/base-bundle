9e944de7ae26c98352edf9c0f3b16384e3be8c0b
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:05:13 +0000

    Added ?ExtensionInterface

--- EveryWorkflowBaseBundle.php
+++ EveryWorkflowBaseBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\BaseBundle;
 
 use EveryWorkflow\BaseBundle\DependencyInjection\BaseExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowBaseBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new BaseExtension();
     }
